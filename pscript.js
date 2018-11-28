ce36cd14e857af09d37f81194c6a8745b76f1294
Balogun Ridwan <balogunridwan@gmail.com>
2018-11-28 13:17:19 +0100

    changes

---
+++ pscript.js
@@ -0,0 +1,5 @@
+// document.getElementById("listTest").innerHTML("This is Test by Ridbay")
+var msg = "Hello World!";
+window.alert(msg);
+console.log("Hello Ridwan");
+document.write("Hello Doc!");
